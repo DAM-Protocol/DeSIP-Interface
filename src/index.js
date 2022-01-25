0e73c1274bf5242aaeb93a919667193985260bcf
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-01-25 20:58:09 +0530

    Moralis Setup

--- src/index.js
+++ src/index.js
@@ -2,13 +2,29 @@ import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
+import { MoralisProvider } from 'react-moralis';
+import { BrowserRouter } from 'react-router-dom';
+
+const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
+const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
+
+const Application = () => {
+	const isServerInfo = APP_ID && SERVER_URL ? true : false;
+	if (isServerInfo)
+		return (
+			<MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+				<BrowserRouter>
+					<App isServerInfo />
+				</BrowserRouter>
+			</MoralisProvider>
+		);
+};
 
 ReactDOM.render(
 	<StrictMode>
 		<ColorModeScript />
-		<App />
+		<Application />
 	</StrictMode>,
 	document.getElementById('root')
 );
@@ -21,4 +37,3 @@ serviceWorker.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
