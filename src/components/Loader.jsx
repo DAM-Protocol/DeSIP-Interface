291ac3811c6c595fdcfe32c061e6d9c9a5c9df5b
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-02-02 13:02:16 +0530

    Added Loader Animation

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,11 @@
+import { Flex, Spinner } from '@chakra-ui/react';
+
 const Loader = () => {
-	return <div>Loading</div>;
+	return (
+		<Flex height={'100vh'} justify='center' align='center'>
+			<Spinner size='xl' speed='1s' />
+		</Flex>
+	);
 };
 
 export default Loader;
