64d5b9f023b48a6dc82eda29fda227d7f57bf705
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-02-02 17:37:45 +0530

    Route rendering from Map

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { Suspense } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Routes, Route, Navigate } from 'react-router';
-import Landing from './pages/Landing';
 import extendedTheme from './theme';
 import DefaultLayout from './components/layouts/DefaultLayout';
 import Loader from './components/Loader';
-import NotFound from './pages/NotFound';
-import Suite from './pages/SuperSuite/Suite';
+import React, { lazy } from 'react';
 
 const App = () => {
 	return (
@@ -14,35 +12,16 @@ const App = () => {
 			<DefaultLayout>
 				<Routes>
 					<Route path='/'>
-						<Route
-							exact
-							path=''
-							element={
-								<Suspense fallback={<Loader />}>
-									<Landing />
-								</Suspense>
-							}
-						/>
-
-						<Route
-							exact
-							path='/Super-Suite'
-							element={
-								<Suspense fallback={<Loader />}>
-									<Suite />
-								</Suspense>
-							}
-						/>
-						<Route
-							exact
-							path='/super-dHEDGE'
-							element={
-								<Suspense fallback={<Loader />}>
-									<Suite />
-								</Suspense>
-							}
-						/>
-						<Route exact path='404' element={<NotFound />} />
+						{paths.map(({ path, component }) => (
+							<Route
+								key={path}
+								path={path}
+								element={
+									<Suspense fallback={<Loader />}>
+										{React.createElement(component)}
+									</Suspense>
+								}></Route>
+						))}
 						<Route path='*' element={<Navigate to='/404' />} />
 					</Route>
 				</Routes>
@@ -50,5 +29,28 @@ const App = () => {
 		</ChakraProvider>
 	);
 };
+
+const paths = [
+	{
+		path: '/',
+		exact: true,
+		component: lazy(() => import('./pages/Landing')),
+	},
+	{
+		path: '/Super-Suite',
+		exact: true,
+		component: lazy(() => import('./pages/SuperSuite/Suite')),
+	},
+	{
+		path: '/Super-dHEDGE',
+		exact: true,
+		component: lazy(() => import('./pages/DHedge')),
+	},
+	{
+		path: '404',
+		exact: true,
+		component: lazy(() => import('./pages/NotFound')),
+	},
+];
 
 export default App;
