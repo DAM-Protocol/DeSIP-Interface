f81bce3497c79f221964c349d454cdcbd69ad91e
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-02-02 13:02:41 +0530

    Page layout style overflow hidden

--- src/components/layouts/Page.jsx
+++ src/components/layouts/Page.jsx
@@ -6,6 +6,8 @@ const Page = (props) => {
 			direction={'column'}
 			minH='calc(100vh - 11rem)'
 			w='100%'
+			maxW={{ xl: '1300px' }}
+			overflowX={'hidden'}
 			justify={['center', 'flex-start']}
 			py='3rem'
 			px='1.5rem'
