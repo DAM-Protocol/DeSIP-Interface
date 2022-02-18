1186a36bdad7dd925d9eb007a7094057f1d754f8
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-02-18 21:01:44 +0530

    Shifted component to containers

--- src/pages/DHedge/index.jsx
+++ src/pages/DHedge/index.jsx
@@ -8,6 +8,7 @@ import {
 	StatNumber,
 	StatHelpText,
 	StatArrow,
+	Box,
 } from '@chakra-ui/react';
 import Pools from '../../components/DHedge/Pools';
 import Page from '../../components/layouts/Page';
@@ -15,39 +16,43 @@ import Page from '../../components/layouts/Page';
 const SuperDHedge = () => {
 	return (
 		<Page>
-			<Heading as='h1' size='2xl'>
-				Super dHEDGE
-			</Heading>
-			<Text>
-				Stream into dHEDGE Pools in two clicks
-				<br />
-				<br />
-				Let top Managers do the work for you. Check their track records and
-				trading strategies.
-			</Text>
-			<StatGroup>
-				<Stat>
-					<StatLabel>Now Streaming</StatLabel>
-					<StatNumber>$ 345,670</StatNumber>
-					<StatHelpText>
-						<StatArrow type='increase' />
-						23.36%
-					</StatHelpText>
-				</Stat>
+			<Box>
+				<Heading as='h1' size='2xl' mb='8'>
+					Super dHEDGE
+				</Heading>
+				<Text display='block'>
+					Stream into dHEDGE Pools in two clicks
+					<br />
+					<br />
+					Let top Managers do the work for you. Check their track records and
+					trading strategies.
+				</Text>
+				<StatGroup>
+					<Stat>
+						<StatLabel>Now Streaming</StatLabel>
+						<StatNumber>$ 345,670</StatNumber>
+						<StatHelpText>
+							<StatArrow type='increase' />
+							23.36%
+						</StatHelpText>
+					</Stat>
 
-				<Stat>
-					<StatLabel>Pools</StatLabel>
-					<StatNumber>4</StatNumber>
-				</Stat>
-			</StatGroup>
+					<Stat>
+						<StatLabel>Pools</StatLabel>
+						<StatNumber>4</StatNumber>
+					</Stat>
+				</StatGroup>
+			</Box>
 
 			<Divider></Divider>
 
-			<Heading as='h2' size='lg'>
-				Pools
-			</Heading>
+			<Box>
+				<Heading as='h2' size='lg' textAlign='center'>
+					Pools
+				</Heading>
 
-			<Pools></Pools>
+				<Pools></Pools>
+			</Box>
 		</Page>
 	);
 };
