e1dede5cb6810bed6bb095e9715e89fc7b63b103
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-05-01 21:04:48 +0530

    [Bug] Default Asset Avatar background colour

--- src/components/AssetTag.jsx
+++ src/components/AssetTag.jsx
@@ -2,7 +2,15 @@ import { Tag, TagLabel, Avatar } from '@chakra-ui/react';
 
 const AssetTag = ({ name, icon }) => (
 	<Tag size='lg' variant='subtle' colorScheme='blue' borderRadius='full' my='2'>
-		<Avatar src={icon} size='xs' name={name} ml={-1} mr={2} loading='lazy' />
+		<Avatar
+			src={icon}
+			backgroundColor='gray.200'
+			size='xs'
+			name={icon}
+			ml={-1}
+			mr={2}
+			colorScheme='blue'
+		/>
 		<TagLabel>{name}</TagLabel>
 	</Tag>
 );
