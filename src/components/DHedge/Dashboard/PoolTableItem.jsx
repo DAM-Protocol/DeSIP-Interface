8ae4f2c2388ccafce6fc094e4df14a445e322162
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-05-04 17:25:30 +0530

    [Feat] Dashboard Overhaul

--- src/components/DHedge/Dashboard/PoolTableItem.jsx
+++ src/components/DHedge/Dashboard/PoolTableItem.jsx
@@ -1,23 +1,24 @@
 import {
-	Tag,
-	Text,
 	useColorModeValue,
 	Grid,
 	VStack,
-	Tooltip,
-	Icon,
-	Flex,
-	HStack,
 	Image,
 	Stack,
-	useDisclosure,
 	Button,
 	Box,
+	Divider,
+	StatGroup,
+	Stat,
+	StatLabel,
+	StatNumber,
+	StatHelpText,
+	Flex,
+	HStack,
 } from '@chakra-ui/react';
-import { AiOutlineInfoCircle } from 'react-icons/ai';
-import InvestModal from '../InvestModal';
 import { InternalLink } from '../../Links';
-import PoolTablePanel from './PoolTablePanel';
+import DhptStats from '../DhptStats';
+import StreamTable from './StreamTable';
+import { NavLink, ExternalLink } from '../../sections/Navbar/Links';
 
 const PoolTableItem = ({ poolData }) => {
 	const border = {
@@ -28,122 +29,79 @@ const PoolTableItem = ({ poolData }) => {
 		borderBottom: '1px solid',
 		borderBottomColor: useColorModeValue('gray.200', 'blue.800'),
 	};
-	const { onClose, isOpen, onOpen } = useDisclosure();
 
 	return (
-		<>
-			<InvestModal poolData={poolData} isOpen={isOpen} onClose={onClose} />
-
-			<Box
-				{...border}
-				my='6'
-				bg={useColorModeValue('whiteAlpha.500', 'blackAlpha.400')}
-			>
-				<PoolAccordionButton onOpen={onOpen} poolData={poolData} />
-
-				<PoolTablePanel></PoolTablePanel>
-			</Box>
-		</>
-	);
-};
-
-const PoolAccordionButton = ({ onOpen, poolData }) => {
-	return (
-		<Box py='10' _hover={{ background: 'none' }}>
-			<Grid
-				w='full'
-				templateColumns={{
-					base: 'repeat(2, 1fr)',
-					md: '1.5fr repeat( 3, 1fr)',
-				}}
-				gap='4'
-				rowGap='8'
-				alignItems={{ base: 'end' }}
-				m='auto'
-			>
-				{/* Pool Image, Name and Invest Button */}
-				<Stack
-					spacing='2'
-					justify='center'
-					align='center'
-					direction={['column', 'row', 'row']}
-					alignSelf={{ base: 'end', md: 'center' }}
+		<Box
+			{...border}
+			my='6'
+			bg={useColorModeValue('whiteAlpha.500', 'blackAlpha.400')}
+		>
+			<Box _hover={{ background: 'none' }}>
+				<Flex
+					w='full'
+					gap='4'
+					p='4'
+					justifyContent={{ base: 'space-between', lg: 'space-evenly' }}
+					wrap='wrap'
+					alignItems={{ base: 'end' }}
+					m='auto'
 				>
-					<Image
-						boxSize='16'
-						rounded={'md'}
-						objectFit={'cover'}
-						filter={'drop-shadow(0px 0px 5px #0000001d)'}
-						src={
-							'https://pbs.twimg.com/profile_images/1434774151340773384/ypAN0vSP_200x200.jpg'
-						}
-					/>
-					<VStack>
-						<InternalLink to={`/Super-dHEDGE/${poolData?.address}`}>
-							{poolData?.name || 'Convex Strategies'}
-						</InternalLink>
-						<Button
-							variant='ghost'
-							onClick={(e) => e.preventDefault() || onOpen()}
-							size='sm'
-						>
-							Invest
-						</Button>
-					</VStack>
-				</Stack>
+					{/* Pool Image, Name and Invest Button */}
+					<Stack
+						w={{ base: '100%', lg: 'auto' }}
+						spacing='2'
+						justify='center'
+						align='center'
+						direction={['column', 'row', 'row']}
+						alignSelf={{ base: 'end', md: 'center' }}
+					>
+						<Image
+							boxSize='16'
+							rounded={'md'}
+							objectFit={'cover'}
+							filter={'drop-shadow(0px 0px 5px #0000001d)'}
+							src={poolData?.imageURL}
+						/>
+						<VStack w='25ch'>
+							<InternalLink to={`/Super-dHEDGE/${poolData?.superPoolAddress}`}>
+								{poolData?.name}
+							</InternalLink>
 
-				{/* Pool Token Balance */}
-				<VStack spacing='1'>
-					<Text as='span' fontSize='sm' opacity='0.7'>
-						$150
-					</Text>
-					<Text as='span' fontWeight='medium'>
-						1,000,000
-					</Text>
-					<Flex fontSize='sm' opacity='0.7' as='span' align='center'>
-						Balance
-						<Tooltip label={`DHPTx`} hasArrow aria-label={`DHPTx`}>
-							<Flex as='span' ml='2'>
-								<Icon as={AiOutlineInfoCircle} w={4} h={4} />
-							</Flex>
-						</Tooltip>
-					</Flex>
-				</VStack>
+							<HStack>
+								<NavLink
+									fontSize='sm'
+									to={`/Super-dHEDGE/${poolData?.superPoolAddress}`}
+								>
+									Super Pool
+								</NavLink>
+								<ExternalLink
+									fontSize='sm'
+									to={`/Super-dHEDGE/${poolData?.superPoolAddress}`}
+								>
+									dHEDGE Fund
+								</ExternalLink>
+							</HStack>
+						</VStack>
+					</Stack>
 
-				{/* Total Value Streaming */}
-				<VStack spacing='1'>
-					<Text as='span' fontWeight='medium'>
-						$140k
-					</Text>
-					<Flex fontSize='sm' opacity='0.7' as='span' align='center'>
-						TVS
-						<Tooltip
-							label='Total Value Streaming'
-							hasArrow
-							aria-label='Total Value Streaming'
-						>
-							<Flex as='span' ml='2'>
-								<Icon as={AiOutlineInfoCircle} w={4} h={4} />
-							</Flex>
-						</Tooltip>
-					</Flex>
-				</VStack>
+					{/* Pool Stats */}
+					<Box>
+						<DhptStats poolData={poolData} />
+					</Box>
 
-				{/* Performance Metrics */}
-				<VStack spacing='1' alignSelf={{ base: 'end', md: 'center' }}>
-					<HStack justify='center' spacing='2'>
-						<Tag variant='outline' p='4' py='2' colorScheme='green'>
-							+10%
-							<br />
-							Day
-						</Tag>
-						<Tag variant='outline' p='4' py='2' colorScheme='red'>
-							-5% <br />
-							Year
-						</Tag>
-					</HStack>
-				</VStack>
-			</Grid>
+					<StatGroup px={{ base: '4', md: '10' }} py='6' zIndex='9' minW='20ch'>
+						<Stat>
+							<StatNumber fontSize='xl'>1hr ago</StatNumber>
+							<StatHelpText>Last Deposited</StatHelpText>
+						</Stat>
+					</StatGroup>
+				</Flex>
+			</Box>
+
+			<Divider mb='4' mt='-4' />
+			<Box borderRadius='10' overflow='scroll' pb='4'>
+				<StreamTable poolData={poolData} />
+			</Box>
 		</Box>
 	);
 };
--- src/components/DHedge/Dashboard/index.jsx
+++ src/components/DHedge/Dashboard/index.jsx
@@ -1,16 +1,30 @@
 import { Box } from '@chakra-ui/react';
+import { useContext, useEffect } from 'react';
+import { useMoralis } from 'react-moralis';
+import { Web3Context } from '../../../context/Web3Context';
 
 import PoolTableItem from './PoolTableItem';
 
 const DHedgeDashboard = () => {
+	const { pools, getDhedgePools, superfluidProvider, initialiseSf } =
+		useContext(Web3Context);
+
+	const { isWeb3Enabled } = useMoralis();
+	useEffect(() => {
+		if (!superfluidProvider && isWeb3Enabled) initialiseSf();
+	}, [isWeb3Enabled, initialiseSf, superfluidProvider]);
+
+	useEffect(() => {
+		if (getDhedgePools) getDhedgePools();
+	}, [getDhedgePools]);
+
 	return (
 		<Box overflowX='auto'>
 			{/* Pool Table */}
 
-			<PoolTableItem />
-			<PoolTableItem />
-			<PoolTableItem />
-			<PoolTableItem />
+			{pools?.map((pool, index) => (
+				<PoolTableItem key={pool.id} poolData={pool.attributes} />
+			))}
 		</Box>
 	);
 };
