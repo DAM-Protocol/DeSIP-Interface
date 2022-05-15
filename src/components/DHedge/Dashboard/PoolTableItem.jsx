048c9e64824a73a89eab4b8ecf8057448155cdc8
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-05-15 22:24:56 +0530

    [Refactor] Split component

--- src/components/DHedge/Dashboard/PoolTableItem.jsx
+++ src/components/DHedge/Dashboard/PoolTableItem.jsx
@@ -34,74 +34,85 @@ const PoolTableItem = ({ poolData }) => {
 			my='6'
 			bg={useColorModeValue('whiteAlpha.500', 'blackAlpha.400')}
 		>
-			<Box _hover={{ background: 'none' }}>
-				<Flex
-					w='full'
-					gap='4'
-					p='4'
-					justifyContent={{ base: 'space-between', lg: 'space-evenly' }}
-					wrap='wrap'
-					alignItems={{ base: 'end' }}
-					m='auto'
-				>
-					{/* Pool Image, Name and Invest Button */}
-					<Stack
-						w={{ base: '100%', lg: 'auto' }}
-						spacing='2'
-						justify='center'
-						align='center'
-						direction={['column', 'row', 'row']}
-						alignSelf={{ base: 'end', md: 'center' }}
-					>
-						<Image
-							boxSize='16'
-							rounded={'md'}
-							objectFit={'cover'}
-							filter={'drop-shadow(0px 0px 5px #0000001d)'}
-							src={poolData?.imageURL}
-						/>
-						<VStack w='25ch'>
-							<InternalLink to={`/Super-dHEDGE/${poolData?.superPoolAddress}`}>
-								{poolData?.name}
-							</InternalLink>
-
-							<HStack>
-								<NavLink
-									fontSize='sm'
-									to={`/Super-dHEDGE/${poolData?.superPoolAddress}`}
-								>
-									Super Pool
-								</NavLink>
-								<ExternalLink
-									fontSize='sm'
-									href={`https://app.dhedge.org/pool/${poolData?.dhedgeFundAddress}`}
-								>
-									dHEDGE Fund
-								</ExternalLink>
-							</HStack>
-						</VStack>
-					</Stack>
-
-					{/* Pool Stats */}
-					<Box>
-						<DhptStats poolData={poolData} />
-					</Box>
-
-					<StatGroup px={{ base: '4', md: '10' }} py='6' zIndex='9' minW='20ch'>
-						<Stat>
-							<StatNumber fontSize='xl'>
-								{getRelativeTime(poolData?.lastDeposit)}
-							</StatNumber>
-							<StatHelpText>Last Deposited</StatHelpText>
-						</Stat>
-					</StatGroup>
-				</Flex>
-			</Box>
+			<ItemHeader poolData={poolData} />
 
 			<Divider mb='4' mt='-4' />
+
 			<Box borderRadius='10' overflow='scroll' pb='4'>
 				<StreamTable poolData={poolData} />
 			</Box>
+		</Box>
+	);
+};
+
+const ItemHeader = ({ poolData }) => {
+	return (
+		<Box _hover={{ background: 'none' }}>
+			<Flex
+				w='full'
+				gap='4'
+				p='4'
+				justifyContent={{ base: 'space-between', lg: 'space-evenly' }}
+				wrap='wrap'
+				alignItems={{ base: 'end' }}
+				m='auto'
+			>
+				{/* Pool Image, Name and Invest Button */}
+				<Stack
+					w={{ base: '100%', lg: 'auto' }}
+					spacing='2'
+					justify='center'
+					align='center'
+					direction={['column', 'row', 'row']}
+					alignSelf={{ base: 'end', md: 'center' }}
+				>
+					<Image
+						boxSize='16'
+						rounded={'md'}
+						objectFit={'cover'}
+						filter={'drop-shadow(0px 0px 5px #0000001d)'}
+						src={poolData?.imageURL}
+					/>
+					<VStack w='25ch'>
+						<InternalLink to={`/Super-dHEDGE/${poolData?.superPoolAddress}`}>
+							{poolData?.name}
+						</InternalLink>
+
+						<HStack>
+							<NavLink
+								fontSize='sm'
+								to={`/Super-dHEDGE/${poolData?.superPoolAddress}`}
+							>
+								Super Pool
+							</NavLink>
+							<ExternalLink
+								fontSize='sm'
+								href={`https://app.dhedge.org/pool/${poolData?.dhedgeFundAddress}`}
+							>
+								dHEDGE Fund
+							</ExternalLink>
+						</HStack>
+					</VStack>
+				</Stack>
+
+				{/* Pool Stats */}
+				<Box>
+					<DhptStats
+						adjustedTokenPrice={poolData?.adjustedTokenPrice}
+						poolSuperToken={poolData?.superToken}
+						prevAdjustedTokenPrice={poolData?.prevAdjustedTokenPrice}
+					/>
+				</Box>
+
+				<StatGroup px={{ base: '4', md: '10' }} py='6' zIndex='9' minW='20ch'>
+					<Stat>
+						<StatNumber fontSize='xl'>
+							{getRelativeTime(poolData?.lastDeposit)}
+						</StatNumber>
+						<StatHelpText>Last Deposited</StatHelpText>
+					</Stat>
+				</StatGroup>
+			</Flex>
 		</Box>
 	);
 };
