3aab041889d5ec94b52256af46dc59a638ab5aed
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-07-12 15:25:10 +0530

    refactor: removed unnecessary wallet connectors

--- src/components/sections/Navbar/connectorConfig.js
+++ src/components/sections/Navbar/connectorConfig.js
@@ -1,10 +1,5 @@
 import Metamask from './WalletIcons/metamaskWallet.png';
-import Coin98 from './WalletIcons/Coin98.png';
 import WalletConnect from './WalletIcons/wallet-connect.svg';
-import MathWallet from './WalletIcons/MathWallet.svg';
-import TokenPocket from './WalletIcons/TokenPocket.svg';
-import SafePal from './WalletIcons/SafePal.svg';
-import TrustWallet from './WalletIcons/TrustWallet.png';
 
 export const connectors = [
 	{
@@ -18,35 +13,5 @@ export const connectors = [
 		icon: WalletConnect,
 		connectorId: 'walletconnect',
 		priority: 2,
-	},
-	{
-		title: 'Trust Wallet',
-		icon: TrustWallet,
-		connectorId: 'injected',
-		priority: 3,
-	},
-	{
-		title: 'MathWallet',
-		icon: MathWallet,
-		connectorId: 'injected',
-		priority: 999,
-	},
-	{
-		title: 'TokenPocket',
-		icon: TokenPocket,
-		connectorId: 'injected',
-		priority: 999,
-	},
-	{
-		title: 'SafePal',
-		icon: SafePal,
-		connectorId: 'injected',
-		priority: 999,
-	},
-	{
-		title: 'Coin98',
-		icon: Coin98,
-		connectorId: 'injected',
-		priority: 999,
 	},
 ];
